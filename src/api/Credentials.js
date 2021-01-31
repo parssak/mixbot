3c8e744c65645bf5b06878f6d048d5906a3e57e9
parssa <parssak@gmail.com>
2021-01-30 20:25:22 -0500

    temp added creds

---
+++ src/api/Credentials.js
@@ -0,0 +1,11 @@
+const cred2 = {
+    ClientId: '75cbbf8c1ff14b9abe3cbfe6924fb5f0',
+    ClientSecret: 'da55e142b302460cab9b737153b93eb4'
+}
+
+
+function Credentials() {
+    return cred2;
+}
+
+export { Credentials };
